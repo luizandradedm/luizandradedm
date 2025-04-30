9e6e130fb716e12a2c6d3af7d2e40c615f9d2d9c
Andraddes <luizandradedm@hotmail.com>
2025-04-30 12:37:28 -0300

    Update dados.lua

--- dados.lua
+++ dados.lua
@@ -1,18 +1,1 @@
--- Simulador de Dados de RPG em Lua
-math.randomseed(os.time())
 
-print("🎲 Bem-vindo ao simulador de dados de RPG!")
-io.write("Quantos dados você quer rolar? ")
-local qtd_dados = tonumber(io.read())
-
-io.write("Quantos lados cada dado deve ter? (ex: 6, 20, etc.): ")
-local lados = tonumber(io.read())
-
-print("\nRolando " .. qtd_dados .. " dado(s) de " .. lados .. " lados...\n")
-
-for i = 1, qtd_dados do
-    local resultado = math.random(1, lados)
-    print("🎯 Dado " .. i .. ": " .. resultado)
-end
-
-print("\nBoa sorte na sua aventura! 🧙‍♂️🗡️🐉")
